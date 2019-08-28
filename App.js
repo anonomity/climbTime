3cf675957963df7a028d33f45819d066e5ad6ef5
Jackelyn Velez <jlauravelez@gmail.com>
2019-08-28 10:10:19 +0200

    Initial Commit for App.js, package.json, and yarn.lock

--- App.js
+++ App.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator, createAppContainer} from 'react-navigation'
-import * as firebase from 'firebase';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import * as firebase from "firebase";
 
-import HomeScreen from './screens/HomeScreen';
-import LoadingScreen from './screens/LoadingScreen';
-import SignInScreen from './screens/SignInScreen';
-import SignUpScreen from './screens/SignUpScreen';
+import HomeScreen from "./screens/HomeScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import SignInScreen from "./screens/SignInScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import AddProgress from "./screens/AddProgress";
+import Finances from "./screens/Finances";
 
+
+// Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyDFAP9vKpgd5E9-5IrN2K_F4rm36gMuYwU",
   authDomain: "climbtime-62821.firebaseapp.com",
   databaseURL: "https://climbtime-62821.firebaseio.com",
   projectId: "climbtime-62821",
-  storageBucket: "",
+  storageBucket: "climbtime-62821.appspot.com",
   messagingSenderId: "386331107411",
   appId: "1:386331107411:web:c61710c6579202a7"
 };
+// Initialize Firebase
 
 firebase.initializeApp(firebaseConfig);
-
 const MainNavigator = createStackNavigator(
   {
-
-  Loading : {screen: LoadingScreen}, 
-  Signin : {screen: SignInScreen}, 
-  Signup : {screen: SignUpScreen}, 
-  Home : {screen: HomeScreen}, 
+    Loading: { screen: LoadingScreen },
+    Signin: { screen: SignInScreen },
+    Signup: { screen: SignUpScreen },
+    Home: { screen: HomeScreen },
+    Graph: { screen: AddProgress },
+    Financez: {screen: Finances}
+    
   },
   {
     initialRouteName: "Loading"
   }
-)
+);
 
 const App = createAppContainer(MainNavigator);
 export default App;
